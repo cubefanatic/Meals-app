ad20f419db1a30551141798ad0bdd2e843230d88
cubefanatic <66870627+cubefanatic@users.noreply.github.com>
2021-06-08 19:57:18 +0400

    Initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton onClick={props.onShowCart}>Cart</HeaderCartButton>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="img" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
